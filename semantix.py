54fd292eb3d9d2ad999f45e53ba7717c4e944f2c
Jeferson de Souza <jfnsouzasantos@gmail.com>
2017-10-18 04:43:46 -0200

    Add files via upload

--- semantix.py
+++ semantix.py
@@ -1,7 +1,5 @@
 
-from pyspark.sql import Row
 
-from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 
 
@@ -13,34 +11,48 @@ conf = SparkConf().setMaster('local').setAppName('semantix')
 
 sc = SparkContext(conf=conf)
 
+resultados = {}
+
 '''
 carrega as bases de dados
 
 '''
-rawData1 = sc.textFile("access_log_Aug95,access_log_Jul95")
+#rawData1 = sc.textFile("access_log_Aug95")
 
-#rawData2 = sc.textFile("access_log_Jul95")
+rawData2 = sc.textFile("access_log_Jul95,access_log_Aug95")
 
-data1 = rawData1.map(lambda x: x.split(" "))#.collect #retorn list
+#data1 = rawData1.map(lambda x: x.split(" "))#.collect #retorn list
+
+data2 = rawData2.map(lambda x: x.split(" ")).filter(lambda x:len(x) > 2)#.collect #retorn list
+
 
 '''
 numero de hosts unicos
 '''
-#exe1 = data1.map(lambda x:x[0]).distinct().count()
+exe1 = data2.map(lambda x:x[0]).distinct().count()
+
+resultados['Host Unicos'] = exe1
+
+print 'Resultado 1:', exe1
 
 '''
 total de retornos 404
 
 '''
-#exe2 = data1.filter(lambda x:x[-2] =='404' ).count()
+
+exe2 = data2.filter(lambda x:x[-2] =='404' ).count()
+
+resultados['Total Retornos 404'] = exe2
+
+print 'Resultado 2',exe2
 
 '''
 As 5 urls que contem mais retornos 404
 
 '''
 
-'''
-dataPair1 = rawData1.map(lambda x: (x.split(" ")[0],x.split(" ")[-2])).filter(lambda keyValue: keyValue[1]=='404')
+
+dataPair1 = data2.map(lambda x: (x[0],x[-2])).filter(lambda keyValue: keyValue[1]=='404')
 
 dict = dataPair1.countByKey()
 
@@ -50,30 +62,49 @@ dict = dict.flatMap(lambda x: x.items())
 
 exe3 = dict.map(lambda x: (x[1], x[0])).sortByKey(ascending =False).take(5)
 
-'''
+resultados['top 5  404'] = exe3
+
+print 'Resultado 3 ', exe3
 
 '''
 retornos 404 por dia
 
 '''
 
-'''
-errosDia = data1.map(lambda x: (x[3][1:7], x[-2])).filter(lambda x:x[1]=='404').countByKey()
+
+errosDia = data2.map(lambda x: (x[3][1:7], x[-2])).filter(lambda x:x[1]=='404').countByKey()
 
 errosDia = sc.parallelize([errosDia])
 
 exe4 = errosDia.flatMap(lambda x:x.items()).sortByKey()
 
-print exe4.take(exe4.count())
-'''
+resultados['erros dia'] = exe4.take(exe4.count())
+
+print 'Resultado 4', exe4.take(exe4.count())
 
 
 '''
 total de bytes retornados
 
 '''
-exe4 = data1.filter(lambda x:x[-1] != '-').map(lambda x:int(x[-1])).reduce(lambda x, y: x+y)
 
-print exe4
+exe5 = data2.filter(lambda x:x[-1] != '-').map(lambda x:int(x[-1])).reduce(lambda x, y: x+y)
+
+resultados['total bytes'] = exe5
+
+print 'Resultado 5',  exe5
+
+
+
+
+'''
+Salva Resultado
+
+'''
+rddResult = sc.parallelize([resultados])
+
+rddResult = rddResult.flatMap(lambda x:x.items())
+
+rddResult.saveAsTextFile('Resultados Finais.txt')
 
 print 'Acabou'
